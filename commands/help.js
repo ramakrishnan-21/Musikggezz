c959ae603bc05d6e41ed8b0c2895dbda4d6a309d
ramakrishnan-21 <54468918+ramakrishnan-21@users.noreply.github.com>
2021-03-23 20:59:12 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,15 +8,18 @@ const emb = new Discord.MessageEmbed()
     "Note that only the first word of the command is important, the rest of the command is your query and what you wish.\n"
 )
 .addFields(
-    { name: "Play a song", value: "`!play <song name>`" },
-    { name: "Pause a song", value: "`!pause`" },
-    { name: "Check bot ping", value: "`!ping`" },
-    { name: "List of songs", value: "`!queue`" },
-    { name: "Resume a song", value: "`!resume`" },
-    { name: "Greeting message", value: "`!hello`" },
-    { name: "Skip a song", value: "`!skip`" },
-    { name: "Disconnect", value: "`!dc`" },
-    { name: "Loop a song/queue again", value: "`!loop <one/full/off>`" }
+    { name: "Play a song", value: "`&play <song name>`" },
+    { name: "Pause a song", value: "`&pause`" },
+    { name: "Check bot ping", value: "`&ping`" },
+    { name: "List of songs", value: "`&queue`" },
+    { name: "Resume a song", value: "`&resume`" },
+    { name: "Greeting message", value: "`&hello`" },
+    { name: "Skip a song", value: "`&skip`" },
+    { name: "Disconnect", value: "`&dc`" },
+    { name: "Loop a song/queue again", value: "`&loop <one/full/off>`" },
+    { name: "About me", value: "`&about`" },
+    
+    
 );
 message.channel.send(emb);
 }
